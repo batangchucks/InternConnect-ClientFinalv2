c8ce2ea8a8068b8e9724defa3ea0038a0c507c9f
JVLandayan <57561054+JVLandayan@users.noreply.github.com>
2021-10-15 01:51:20 +0800

    pdfstate, resetyear, academicyearupdate

--- src/app/pages/admin/components/admin-acadyear/admin-acadyear.component.ts
+++ src/app/pages/admin/components/admin-acadyear/admin-acadyear.component.ts
@@ -1,6 +1,8 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AcademicyearService } from 'src/app/shared/services/academicyear.service';
+import { fileUpload } from 'src/app/shared/services/fileUpload.service';
 
 @Component({
   selector: 'app-admin-acadyear',
@@ -8,10 +10,13 @@ import { AcademicyearService } from 'src/app/shared/services/academicyear.servic
   styleUrls: ['./admin-acadyear.component.scss'],
 })
 export class AdminAcadyearComponent implements OnInit {
+  user = JSON.parse(localStorage.getItem('user'));
   AcadYearIndicator: boolean = false;
   constructor(
     private date: DatePipe,
-    private academicyear: AcademicyearService
+    private academicyear: AcademicyearService,
+    private File: fileUpload,
+    private router: Router
   ) {}
   _ayId: number;
   _ayCollegeName: string;
@@ -19,6 +24,12 @@ export class AdminAcadyearComponent implements OnInit {
   _ayStartDate: string;
   _ayEndDate: string;
 
+  selectedFileUpOne: File = null;
+  selectedFileUpTwo: File = null;
+  imageSrc: string;
+  univPhotoFileName: string;
+
+  _psId: number;
   _psIgaarpName: string;
   _psDeanName: string;
   _psUstLogoFileName: string;
@@ -35,24 +46,103 @@ export class AdminAcadyearComponent implements OnInit {
       this._ayStartDate = this.date.transform(resp.startDate, 'yyyy-MM-dd');
       this._ayEndDate = this.date.transform(resp.endDate, 'yyyy-MM-dd');
     });
+
+    this.academicyear.getPdfState().subscribe((resp) => {
+      this._psId = resp.id;
+      this._psIgaarpName = resp.igaarpName;
+      this._psDeanName = resp.deanName;
+      this._psUstLogoFileName = resp.ustLogoFileName;
+      this._psCollegeLogoFileName = resp.collegeLogoFileName;
+    });
   }
-  toConfirm() {
+  toConfirmReset() {
+    var resetPayload = {
+      email: this.user.email,
+      password: this._deanPassword,
+    };
+    console.log(resetPayload);
+    this.academicyear
+      .resetYear(this._ayStartDate, this._ayEndDate, resetPayload)
+      .subscribe(
+        (resp) => {
+          this.router.navigate(['/admin']);
+        },
+        (err: Error) => console.log(err.message)
+      );
+  }
+
+  toReset() {
+    this.AcadYearIndicator = true;
+  }
+
+  toCancel() {
+    this.AcadYearIndicator = false;
+  }
+
+  toUpdate() {
     var ayPayload = {
+      id: this._ayId,
       collegeName: this._ayCollegeName,
-      startDate: this._ayStartDate,
-      endDate: this._ayEndDate,
       igaarpEmail: this._ayIgaarpEmail,
     };
+    this.academicyear.updateAcademicYear(ayPayload).subscribe((resp) => {});
 
     var psPayload = {
+      id: this._psId,
       igaarpName: this._psIgaarpName,
       deanName: this._psDeanName,
       ustLogoFileName: this._psUstLogoFileName,
       collegeLogoFileName: this._psCollegeLogoFileName,
     };
+
+    this.academicyear.updatePdfState(psPayload).subscribe((resp) => {
+      alert('Successfully updated');
+      this.router.navigate(['/admin']);
+      this.ngOnInit();
+    });
   }
 
-  toCancel() {
-    this.AcadYearIndicator = false;
+  UnivLogoUp(event) {
+    this.selectedFileUpOne = <File>event.target.files[0];
+    const reader = new FileReader();
+    if (event.target.files && event.target.files.length) {
+      const [image] = event.target.files;
+      reader.readAsDataURL(image);
+      reader.onload = () => {
+        this.imageSrc = reader.result as string;
+      };
+    }
+    const formData: FormData = new FormData();
+    formData.append(
+      'files',
+      this.selectedFileUpOne,
+      this.selectedFileUpOne.name
+    );
+
+    this.File.uploadLanding(formData).subscribe((data: any) => {
+      this._psUstLogoFileName = data.toString();
+    });
+  }
+
+  CollegeLogoUp(event) {
+    this.selectedFileUpTwo = <File>event.target.files[0];
+    const reader = new FileReader();
+    if (event.target.files && event.target.files.length) {
+      const [image] = event.target.files;
+      reader.readAsDataURL(image);
+      reader.onload = () => {
+        this.imageSrc = reader.result as string;
+      };
+    }
+    const formData: FormData = new FormData();
+    formData.append(
+      'files',
+      this.selectedFileUpTwo,
+      this.selectedFileUpTwo.name
+    );
+
+    this.File.uploadLanding(formData).subscribe((data: any) => {
+      this._psCollegeLogoFileName = data.toString();
+    });
   }
 }
--- src/app/shared/services/academicyear.service.ts
+++ src/app/shared/services/academicyear.service.ts
@@ -1,13 +1,14 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
+import { fileUpload } from './fileUpload.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AcademicyearService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private fileUpload: fileUpload) {}
   private apiUrl = environment.apiUrl;
   getAcademicYear(): Observable<any> {
     return this.http.get<any>(this.apiUrl + 'api/AcademicYear');
@@ -15,5 +16,27 @@ export class AcademicyearService {
 
   getPdfState(): Observable<any> {
     return this.http.get<any>(this.apiUrl + 'api/pdfState');
+  }
+
+  resetYear(startDate: string, endDate: string, payload: any): Observable<any> {
+    // let header = new HttpHeaders({
+    //   'content-type': 'application/json-patch+json',
+    // });
+    return this.http.post<any>(
+      this.apiUrl +
+        'api/Accounts/DeleteAll?startDate=' +
+        startDate +
+        '&endDate=' +
+        endDate,
+      payload
+    );
+  }
+
+  updateAcademicYear(ayPayload: any): Observable<any> {
+    return this.http.put(this.apiUrl + 'api/AcademicYear', ayPayload);
+  }
+
+  updatePdfState(psPayload: any): Observable<any> {
+    return this.http.put(this.apiUrl + 'api/PdfState', psPayload);
   }
 }
