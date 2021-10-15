b17777f3edecb05faa7c541a4d4f03fc85e9931c
JVLandayan <57561054+JVLandayan@users.noreply.github.com>
2021-10-15 11:46:45 +0800

    admin-dashboard update

--- src/app/pages/admin/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/pages/admin/components/admin-dashboard/admin-dashboard.component.ts
@@ -26,7 +26,6 @@ export class AdminDashboardComponent implements OnInit {
   ngOnInit(): void {
     this.dashboard.getStudentList().subscribe((resp) => {
       this.studentList = resp;
-      console.log(resp);
       this.studentsWithCompany = resp.filter((student) => {
         return (
           student.submissions.slice(-1)[0].adminResponse.companyAgrees == true
@@ -35,38 +34,34 @@ export class AdminDashboardComponent implements OnInit {
       this.studentsWithoutCompany = resp.filter((student) => {
         return student.submissions == null;
       }).length;
-      console.log(resp);
 
       this.studentsWithRequests = resp.filter((student) => {
         return (
           student.submissions.slice(-1)[0].adminResponse.companyAgrees == false
         );
       }).length;
-    });
 
-    this.chart = document.getElementById('status_chart');
-    Chart.register(...registerables);
-    this.loadStudentsChart(
-      this.studentsWithCompany,
-      this.studentsWithoutCompany
-    );
+      this.chart = document.getElementById('status_chart');
+      Chart.register(...registerables);
+      this.loadStudentsChart(
+        this.studentsWithCompany,
+        this.studentsWithoutCompany
+      );
+    });
 
     this.chart = document.getElementById('company_chart');
     Chart.register(...registerables);
     this.loadCompanyChart();
   }
 
-  loadStudentsChart(
-    studentsWithCompany: number,
-    studentsWithoutCompany: number
-  ): void {
+  loadStudentsChart(withCompany: number, withoutCompany: number): void {
     new Chart(this.chart, {
       type: 'doughnut',
       data: {
         labels: ['Students with Company', 'Students without company'],
         datasets: [
           {
-            data: [studentsWithCompany, 250],
+            data: [withCompany, withoutCompany],
             backgroundColor: ['#F4D35E', '#73A580'],
           },
         ],
